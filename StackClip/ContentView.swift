356ea88c40207981b61beda40317b14d8b0efdeb
MioShirakawa <abcgfsmile@gmail.com>
2025-06-03 15:08:17 +0200

    Initial Commit

---
+++ StackClip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StackClip
+//
+//  Created by Shirakawa Mio on 03.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StackClip/StackClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackClipApp.swift
+//  StackClip
+//
+//  Created by Shirakawa Mio on 03.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct StackClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
